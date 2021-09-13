fe8afd88775c124df82ceca36b8fe125d372c825
mehedi1722 <mh514928@gmail.com>
2021-09-13 14:00:22 +0600

    Added some functionality

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const Title = document.getElementById('Title')
 const Author = document.getElementById('Author')
 const Year = document.getElementById('Year')
 const Submit = document.getElementById('Submit')
+const TableBody = document.getElementById('TableBody')
 
 // CLASS FOR BOOK LIST 
 class Book {
@@ -14,12 +15,11 @@ class Book {
     }
 }
 
-class CreateListOfBook extends Book{
+class UI extends Book{
     constructor() {
         super(Title, Author, Year)
     }
-    CreateList(BookInfo) {
-        console.log(BookInfo)
+    static CreateList(BookInfo) {
         const TableBody = document.getElementById('TableBody');
         const BookItem = `
             <tr>
@@ -28,7 +28,7 @@ class CreateListOfBook extends Book{
                 <td>${BookInfo.Year}</td>
                 <td>
                     <a href="#">
-                    <i class="fas fa-edit"></i> <i class="fas fa-trash-alt"></i>
+                    <i class="fas fa-edit"></i> <i id="deleteBtn" class="fas fa-trash-alt"></i>
                     </a>
                 </td>
             </tr>
@@ -38,10 +38,33 @@ class CreateListOfBook extends Book{
         Author.value = '';
         Year.value = '';
     }
+
+    static ShowAlert(alert, warning) {
+        alert.innerText = warning
+        alert.style.display = 'block';
+        setTimeout( () => {
+            alert.style.display = 'none';
+        },3000)
+    }
+
+    static RemoveBookFromList(targetButton, id) {
+        const success = document.querySelector('.success');
+
+        let removeButton = targetButton;
+        if(removeButton.id === id) {
+            let removeItem = removeButton.parentElement.parentElement.parentElement;
+            removeItem.className = "remove_effect"; // Remove effect class
+            removeItem.addEventListener('transitionend', function () {
+                removeItem.remove()
+                UI.ShowAlert(success, 'Successfully Item Deleted')
+            })
+        }
+    }
 }
 
 // ADD EVENTLISTENERS 
 Form.addEventListener('submit', AddBooks);
+TableBody.addEventListener('click', removeBookItem);
 
 // CREATE FUNCTION FOR EVENTS 
 function AddBooks(e) {
@@ -53,21 +76,19 @@ function AddBooks(e) {
     const error = document.querySelector('.error');
     const success = document.querySelector('.success');
 
+    let NewBook = new Book(TitleValue, AuthorValue, YearValue)
+
     if(TitleValue === "" || AuthorValue === "" || YearValue === "") {
-        error.style.display = 'block';
-        setTimeout( () => {
-            error.style.display = 'none';
-        },3000)
+        UI.ShowAlert(error, 'Input field can\'t be empty');
     } else {
-        let NewBook = new Book(TitleValue, AuthorValue, YearValue)
-
-        let Books = new CreateListOfBook();
-        Books.CreateList(NewBook)
-
-        success.style.display = 'block';
-        setTimeout( () => {
-            success.style.display = 'none';
-        },3000)
+        UI.CreateList(NewBook)
+        UI.ShowAlert(success, 'Books Added to the list');
     }
 
+}
+
+// REMOVE ELEMENTS FROM THE LIST 
+function removeBookItem(e) {
+    e.preventDefault()
+    UI.RemoveBookFromList(e.target, "deleteBtn")
 }
